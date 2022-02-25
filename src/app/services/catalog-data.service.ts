33ebd63f74feed4a8e64636375334403bca48778
esteban_89_1 <esteban_89_1@hotmail.com>
2022-02-25 02:47:35 -0300

    fix

--- src/app/services/catalog-data.service.ts
+++ src/app/services/catalog-data.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { catchError, EMPTY, map, Observable, of } from 'rxjs';
 import { urlGlobal, environment } from 'src/environments/environment';
 import { CatalogItem } from '../model/model';
 
@@ -12,6 +12,10 @@ export class CatalogDataService {
   constructor(private http: HttpClient) { }
 
   getAllCatalog(): Observable<CatalogItem[]> {
-    return this.http.get<CatalogItem[]>(environment.apiUrl + urlGlobal.GetAllCatalog);
+    return this.http.get<CatalogItem[]>(environment.apiUrl + urlGlobal.GetAllCatalog).pipe(
+    catchError(e => {
+      console.error(e);
+      return of<Array<CatalogItem>>([]);
+    }));
   }
 }
